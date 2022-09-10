c20b72d44a67c10040e6299b578c0161251c9d16
Matheus <devmatheus@gmail.com>
2022-09-10 11:03:27 -0300

    Aulas ruby 1.

---
+++ module2/lesson1/aula1.rb
@@ -0,0 +1,25 @@
+def printSomething (something)
+    puts something
+  end
+
+def sSum(number1, number2)
+    number1 + number2
+end
+
+def times(number1, number2)
+    number1 * number2
+end
+
+soma = sSum 8,2
+mult = times 158900,8
+
+printSomething soma
+printSomething mult
+
+  printSomething "cachorro";
+
+  i = 3
+  i.times{ puts rand(101) }
+
+  stringToFloat = stringToFloat.to_f
+  puts stringToFloat
