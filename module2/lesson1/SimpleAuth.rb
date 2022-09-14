06c3cb1001fc4a764d7a5c2b82c22b725be022a2
Matheus <devmatheus@gmail.com>
2022-09-14 20:37:59 -0300

    finished exercise simple authenticator

--- module2/lesson1/SimpleAuth.rb
+++ module2/lesson1/SimpleAuth.rb
@@ -12,28 +12,35 @@ line(60,'=')
 puts "This programa will take input from the user and compare password"
 puts "If password is correct you will get back the user object"
 
-# users =    ["user1" => "pass1",
-#             "user2" => "pass2",
-#             "user3" => "pass3"]
+users =    [{username:"user1" ,password: "pass1"},
+            {username:"user2" ,password: "pass2"},
+            {username:"user3" , password: "pass3"}]
 
-# def authenticate(user,password)
-#     if (users.each{|k, v| myHash.select (k) if v.eql)
-#     puts "user logged in"
-#     else 
-#         puts "wrong pass or user, try again."
-# end
+def authenticateUser(username,password,userList)
+    
+    userList.each do |userRecord|
+        if userRecord[:username] == username && userRecord[:password] == password
+            return userRecord
+            break
+        else
+            return "Invalid credentials."
+        end
+    end
+end
 
 attempt = 1
-while attempt < 4
-    puts "Username:"
-    username = gets.chomp
-    puts "Password:"
-    password = gets.chomp
-    puts "Press n do quit or any other key to continue: "
-    input = gets.chomp.downcase
+    while attempt < 4
+        puts "Username:"
+        username = gets.chomp
+        puts "Password:"
+        password = gets.chomp
+        auth = authenticateUser(username,password,users)
+        puts auth
+        puts "Press n do quit or any other key to continue: "
+        input = gets.chomp.downcase
         break if input == "n"
         attempt+=1
-end
+    end
 
 
 
