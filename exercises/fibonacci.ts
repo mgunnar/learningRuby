556a695cfd0dffb26528c5a5158defa5a564ddff
Matheus <devmatheus@gmail.com>
2022-09-27 10:14:38 -0300

    finished.

--- exercises/fibonacci.ts
+++ exercises/fibonacci.ts
@@ -1,26 +1,9 @@
-function do_fibon(input:number){
-
-    let    sequence = [1];
-    for (let i =1;i<input;i++) {
-    sequence[i] = 0;
-    sequence.pop()}
-
-              console.log(sequence);
-        
-    for (let index = 1; index<=input; index++){ 
-        if (sequence[index] > input){
-            console.log("maior que input");
-            break;
-        } else {
-         let x = sequence[index] + sequence[index-1];
-          sequence[index+1] = x;
-          console.log(index);          }
-          //console.log(sequence[index]);
-        }
-
-     sequence.shift();
-    // sequence.pop;
-    return console.log ("The fibonacci sequence until the input number is: "+sequence);
+function doFibon(input:number){
+    let    sequence = [1,1];
+    for (let index = 1; sequence[index]<input; index++) 
+        sequence[index+1] = sequence[index] + sequence[index-1];  
+    sequence.pop();
+    console.log ("The fibonacci sequence until the input number is: ");
+    console.log(sequence);
 }
-
-do_fibon(10);
+doFibon(1000);
