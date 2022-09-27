6d44aac8b38c418091dc626b5850aa0d0b74e38a
Matheus <devmatheus@gmail.com>
2022-09-26 22:10:11 -0300

    Finished the fibonacci exercise.

--- exercises/fibonacci_exercise.rb
+++ exercises/fibonacci_exercise.rb
@@ -1,6 +1,6 @@
 def do_fibon()
     
-    puts "insert a number."
+    puts "Enter the number that will be the limit for the fibonacci sequence."
     input = gets()
 
     sequence = [1]
@@ -10,12 +10,11 @@ def do_fibon()
         if sequence[index].to_i> input.to_i
             break
         end
-
             sequence[index+1] = sequence[index].to_i + sequence[index-1].to_i
-            puts sequence[index]
             index +=1
-
     end
+    sequence.shift(2)
+    sequence.pop
     puts "The fibonacci sequence until the input number is: #{sequence}."
 end
 
