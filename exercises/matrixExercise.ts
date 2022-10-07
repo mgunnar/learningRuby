56e29e51d17bba719f9463d6808a26372a4cacf6
Matheus Gunnar <matheus.silva@spmassets.com>
2022-10-07 12:01:34 -0300

    Finalizando o exercício.

--- exercises/matrixExercise.ts
+++ exercises/matrixExercise.ts
@@ -1,28 +1,40 @@
-let matrix = [
-    
-[1,0,3], 
-[0,2,1], 
-[1,2,0]]
-
+let matrix2 = [
+  [1, 1, 1, 0],
+  [0, 5, 0, 1],
+  [2, 1, 3, 10],
+];
 
 function solution(matrix) {
-    let matrixColumn = matrix.length;
-    let matrixElementsRow = matrix[0].length;
-    let sumMatrix = 0;          
-      for(let x=0; x<matrixColumn; x++){
-          for(let y=0; y<matrixElementsRow; y++){
-              if (x==0 && matrix[x][y]!=0
-              || x !=0 && matrix[x-1][y]!=0 && matrix[x][y]!=0 ) {
-                  console.log("Somou " +  matrix[x][y])
-                  sumMatrix+= matrix[x][y]}   
-                  if (x>1 && matrix[x-1][y]==0 && matrix[x-2][y]==0){      
-                  sumMatrix-= matrix[x][y];    }
-          }
+  let matrixColumn = matrix.length;
+  let matrixElementsRow = matrix[0].length;
+  let sumMatrix = 0;
+  for (let x = 0; x < matrixColumn; x++) {
+    for (let y = 0; y < matrixElementsRow; y++) {
+      if (
+        (x == 0 && matrix[x][y] != 0) ||
+        (x != 0 && matrix[x - 1][y] != 0 && matrix[x][y] != 0) ||
+        (x >= 2 && matrix[x - 1][y] == 0) ||
+        (x >= 2 && matrix[x - 2][y] == 0)
+      ) {
+        console.log("Somou " + matrix[x][y]);
+        sumMatrix += matrix[x][y];
       }
-      return sumMatrix;
+
+      if (
+        (x >= 2 && matrix[x - 1][y] == 0) ||
+        (x >= 2 && matrix[x - 2][y] == 0) ||
+        (x > 2 && matrix[x - 3][y] == 0)
+      ) {
+        console.log("Subtraiu " + matrix[x][y]);
+        sumMatrix -= matrix[x][y];
+      }
+    }
   }
+  return sumMatrix;
+}
 
+// let soma = solution(matrix);
+let soma2 = solution(matrix2);
 
-let soma = solution(matrix);
-            
-console.log (soma);
+// console.log(soma);
+console.log(soma2);
